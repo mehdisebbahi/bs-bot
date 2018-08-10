31f16ea67420f8afedcd3f0c6d6ccacad3489ce1
mehdisebbahi <42147862+mehdisebbahi@users.noreply.github.com>
2018-08-10 13:51:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+console.log("BOT ONLINE");
 
-client.on('message', msg => {
-  if (msg.content === 'ادخلو سيرفرنا') {
-    msg.reply('https://discord.gg/teKnWcX');
-  }
-});
-
-client.login('NDc1Njc2NTI1NDQyODkxNzg4.DkihuA.-wdMifpviFrCiOzlYU1benc3BI8');
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+سلام عليكم
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/teKnWcX ] 
+الدعووة خاصة لك ... [ MedoxYT  ]
+**`)
+}).catch(console.error)
+})
+A7MD.login('NDc0MjE1OTYwMjc5MjUzMDI2.Dki55w.z2EFYO85GfWnRNePrV_u-1y00AU');
